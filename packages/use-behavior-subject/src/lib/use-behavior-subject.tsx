afdc77e5ea78d3b790474357ae32135f35bd7003
czaplej26 <czaplejewicz.krzysztof@gmail.com>
2020-11-27 15:30:09 +0100

    update useBehaviorSubject

--- packages/use-behavior-subject/src/lib/use-behavior-subject.tsx
+++ packages/use-behavior-subject/src/lib/use-behavior-subject.tsx
@@ -64,7 +64,7 @@ export const useBehaviorSubject = <T extends unknown>(props: UseBehaviorSubjectP
 
   const setState = useCallback((value: Partial<T> | T) => {
     comparePrevStateAndNewValue(subject, value);
-  }, []);
+  }, [subject]);
 
   return { state: clone(subject.value), setInitialState, setState };
 };
